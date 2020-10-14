ab87ef64f146d1996ee2ff7c911d4a04f53238cb
teknoprep <cm.rawlings@gmail.com>
2020-10-14 12:39:17 +0000

    Changes to Script to make a few extra variables

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-pdf_folder = r"E:\Workspace\Python Projects\DWG_to_PDF_python\pdf"
-work_folder = r"D:\temp"
-search_folders = [r"D:\1"]
-dp_executable_path = r"D:\Program Files (x86)\Any DWG to PDF Converter Pro\dp.exe"
-
-
 from os.path import join, dirname
 from dotenv import load_dotenv
 import os, dotenv, shutil
 from datetime import datetime
 import subprocess
+
+START_SCRIPT_DATE = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+#Please Update These Variables to Match your System Requirements
+pdf_folder = r"c:\temp\pdfout"
+work_folder = r"c:\temp\dwg_converter"
+search_folders = [r"c:\temp"]
+dp_executable_path = r"C:\Program Files (x86)\Any DWG to PDF Converter Pro\dp.exe"
+dp_option_PDFColor = r"GrayScale"       #Valid Options are TrueColors, GrayScale, BlackWhite
+dp_option_PDFQuality = r"High"          #Valid Options are Normal, Medium, High, Highest
+dp_option_HIDE = r"flase"                #Valid Options are true or false
+
+
+
+print(START_SCRIPT_DATE)
 
 
 dotenv_path = join(dirname(__file__), '.env')
@@ -32,8 +41,13 @@ for search_folder in search_folders:
                         
                 except OSError:                    
                     mtime = 0
-subprocess.call([dp_executable_path, '/InFolder', work_folder, '/OutFolder', pdf_folder, '/ConvertType', 'DWG2PDF'])
+if dp_option_HIDE == 'true':
+        subprocess.call([dp_executable_path, '/InFolder', work_folder, '/OutFolder', pdf_folder, '/ConvertType', 'DWG2PDF', '/PDFColor', dp_option_PDFColor, '/PDFQuality', dp_option_PDFQuality, '/HIDE' ])
+else:
+    subprocess.call([dp_executable_path, '/InFolder', work_folder, '/OutFolder', pdf_folder, '/ConvertType', 'DWG2PDF', '/PDFColor', dp_option_PDFColor, '/PDFQuality', dp_option_PDFQuality ])
+
 shutil.rmtree(work_folder)
 
 
-dotenv.set_key(dotenv_path, "LAST_DATE", str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+#dotenv.set_key(dotenv_path, "LAST_DATE", str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+dotenv.set_key(dotenv_path, "LAST_DATE", START_SCRIPT_DATE)
