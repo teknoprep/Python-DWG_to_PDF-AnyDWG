c340ca526f9defbe1df5be1db1ee26b7f477c7b5
teknoprep <chris@blueuc.com>
2020-12-11 13:14:29 -0500

    Updated it to convert files at time minus 6 hours

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 import os, dotenv, shutil
 from datetime import datetime
+from datetime import timedelta
 import subprocess
 import time
 
 START_SCRIPT_DATE = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+last_date = os.environ.get('LAST_DATE')
+print("Last Date", last_date)
 
 def PDF_Converter():
     START_SCRIPT_DATE = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -17,18 +21,21 @@ def PDF_Converter():
     work_folder = r"e:\temp\dwg_converter"
     search_folders = [r"E:\Vaults\files\Drawings"]
     dp_executable_path = r"C:\Program Files (x86)\Any DWG to PDF Converter Pro\dp.exe"
-    dp_option_PDFColor = r"GrayScale"       #Valid Options are TrueColors, GrayScale, BlackWhite
+    dp_option_PDFColor = r"BlackWhite"      #Valid Options are TrueColors, GrayScale, BlackWhite
     dp_option_PDFQuality = r"High"          #Valid Options are Normal, Medium, High, Highest
     dp_option_HIDE = r"true"                #Valid Options are true or false
     dp_option_RUNMANY = r"false"            #Valid Options are true or fales --- this is setup so that your PDF Generator will run ALL files at once or one at a time
-    rerun_wait_time = float(300)             #Time to wait in seconds before re_running_script --- change the number inside float() --- IF SET TO 0... this script only runs once
-
+    rerun_wait_time = float(300)            #Time to wait in seconds before re_running_script --- change the number inside float() --- IF SET TO 0... this script only runs once
+    
     dotenv_path = join(dirname(__file__), '.env')
     load_dotenv(dotenv_path)
-    last_date = os.environ.get('LAST_DATE')
+    last_run_date = os.environ.get('LAST_DATE')
+    last_date = datetime.strptime(last_run_date, '%Y-%m-%d %H:%M:%S')
+    last_date = last_date - timedelta(hours=6)
+    
 
-    print("Last Date", last_date)
-    print("Start Script Date", START_SCRIPT_DATE)     
+    #print("Last Date", last_date)
+    #print("Start Script Date", START_SCRIPT_DATE)     
        
     
     if os.path.isdir(work_folder):
@@ -41,9 +48,9 @@ def PDF_Converter():
                 if file[-3:].lower() == "dwg":                 
                     try:
                         source_file = root + "\\" + file
-                        mtime = os.path.getctime(source_file)
+                        mtime = os.path.getmtime(source_file)
                         created_date = datetime.fromtimestamp(mtime)
-                        if created_date > datetime.strptime(last_date, '%Y-%m-%d %H:%M:%S'):
+                        if created_date > last_date:
                             shutil.copyfile(source_file, os.path.join(work_folder, file))
                             print("i'll print this file", work_folder + '\\' + file, "to this location as a PDF", pdf_folder + '\\' + file)
                             if dp_option_RUNMANY == 'false':
@@ -57,13 +64,13 @@ def PDF_Converter():
                         mtime = 0
     if dp_option_RUNMANY == 'true':
         subprocess.call([dp_executable_path, '/InFolder', work_folder, '/OutFolder', pdf_folder, '/ConvertType', 'DWG2PDF', '/PDFColor', dp_option_PDFColor, '/PDFQuality', dp_option_PDFQuality, '/RECOVER' ])
-    else:
-        print("why are you not doing everything at once")
+    #else:
+    #   print("why are you not doing everything at once")
 
       
     shutil.rmtree(work_folder)
     dotenv.set_key(dotenv_path, "LAST_DATE", START_SCRIPT_DATE)
-    print("Waiting for", rerun_wait_time, "seconds before running again")
+    #print("Waiting for", rerun_wait_time, "seconds before running again")
     if rerun_wait_time == 0:
         quit()
     else:
